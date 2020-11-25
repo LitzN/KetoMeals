cd2bb484c97b1a7c8e4f1098d1e2d261e9663114
LitzN <alitzianedyalkova@hotmail.co.uk>
2020-11-25 12:36:24 +0000

    Added my recipes page

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("Registration complete!")
+        return redirect(url_for("my_recipes", username=session["user"]))
     return render_template("register.html")
 
 
@@ -56,8 +57,10 @@ def login():
         if existing_user:
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for(
+                        "my_recipes", username=session["user"]))
             else:
                 flash("Invalid username/password")
                 return redirect(url_for("login"))
@@ -65,6 +68,13 @@ def login():
             flash("Invalid username/password")
             return redirect(url_for("login"))
     return render_template("login.html")
+
+
+@app.route("/my_recipes/<username>", methods=["GET", "POST"])
+def my_recipes(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("my_recipes.html", username=username)
 
 
 # CHANGE DEBUG TO FALSE WHEN PROJECTS DONE
