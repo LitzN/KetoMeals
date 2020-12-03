9b4cd1159b36fdbc3bf115036db32a3dfc12d9c6
LitzN <alitzianedyalkova@hotmail.co.uk>
2020-12-03 18:01:47 +0000

    Added functionality to meal type buttons

--- static/js/script.js
+++ static/js/script.js
@@ -3,24 +3,22 @@
     $('select').formSelect();
     $('.collapsible').collapsible();
 
-    /*
-
-    $(".open-meal").click(function(){
-    $(".mains").removeClass('hide');
-    $(".snack").addClass('hide')
-    $(".desserts").addClass('hide')
+    $(".open-main").click(function(){
+    $(".mains").removeClass("hide");
+    $(".snacks").addClass("hide");
+    $(".desserts").addClass("hide")
 })
 
     $(".open-snack").click(function(){
-    $(".snacks").removeClass('hide');
-    $(".mains").addClass('hide')
-    $(".desserts").addClass('hide')
+    $(".snacks").removeClass("hide");
+    $(".mains").addClass("hide");
+    $(".desserts").addClass("hide")
 })
 
 $(".open-dessert").click(function(){
-    $(".dessert").removeClass('hide');
-    $(".mains").addClass('hide')
-    $(".snack").addClass('hide')
-}) */
+    $(".desserts").removeClass("hide");
+    $(".mains").addClass("hide");
+    $(".snack").addClass("hide")
+})
 })
 
